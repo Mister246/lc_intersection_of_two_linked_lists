f54ce041c9898a0143356ab99fc4944a79c2e8a1
Joel Elrod <joelelrod246@gmail.com>
2023-07-07 14:44:21 -0400

    First attempt
    Signed-off-by: Joel Elrod <joelelrod246@gmail.com>

--- lc_intersection_of_two_linked_lists/Program.cs
+++ lc_intersection_of_two_linked_lists/Program.cs
@@ -12,6 +12,21 @@ public class Solution
 {
     static public ListNode GetIntersectionNode(ListNode headA, ListNode headB)
     {
+        for (ListNode head = headB; headA != null; headA = headA.next)
+        // for each node in headA
+        {
+            while (headB != null)
+            // for every other node in headB
+            {
+                if (headA == headB)
+                {
+                    return headA;
+                }
+                headB = headB.next;
+            }
+            headB = head;
+        }
 
+        return null;
     }
 }
