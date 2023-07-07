9b9a9c476a1c6b98d9006c2ecdabc9f482369286
Joel Elrod <joelelrod246@gmail.com>
2023-07-07 14:25:24 -0400

    Initial Commit
    Signed-off-by: Joel Elrod <joelelrod246@gmail.com>

---
+++ lc_intersection_of_two_linked_lists/Program.cs
@@ -0,0 +1,17 @@
+﻿
+
+
+public class ListNode 
+{
+    public int val;
+    public ListNode next;
+    public ListNode(int x) { val = x; }
+}
+
+public class Solution
+{
+    static public ListNode GetIntersectionNode(ListNode headA, ListNode headB)
+    {
+
+    }
+}
